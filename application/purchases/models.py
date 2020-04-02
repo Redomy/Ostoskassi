fcb7ee63260e9eeb8f53184525c6ec60f5fe3960
Onni Ovaskainen <onni.ovaskainen@helsinki.fi>
2020-04-02 18:28:52 +0300

    Base class ja tyylittelyä bootstrap

--- application/purchases/models.py
+++ application/purchases/models.py
@@ -1,10 +1,7 @@
 from application import db
+from application.models import Base
 
-class Purchase(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+class Purchase(Base):
 
     name = db.Column(db.String(144), nullable=False)
     amount = db.Column(db.Integer, nullable=False)
